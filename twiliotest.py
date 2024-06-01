3b5f40d076dd08e4b7e1f0501294df994d7ba4b8
Alex Huang <alexahuanga1029@gmail.com>
2024-06-01 13:08:37 -0700

    online github copy with both files

--- twiliotest.py
+++ twiliotest.py
@@ -2,15 +2,15 @@ import os
 from twilio.rest import Client
 
 
-account_sid = "ACf4b80158623dd36f09d7f95dac4e0724"
-auth_token = "6972f9c7d1f2dfc091a1e50325be8b2d"
+account_sid = ""
+auth_token = ""
 client = Client(account_sid, auth_token)
 
 message = client.messages \
                 .create(
                     body="Hello. Testing 123 123 123!~@($)#$()",
                     from_='+18552168577',
-                    to='+14807498123'
+                    to='**********'
                 )
 
 print(message.sid)
