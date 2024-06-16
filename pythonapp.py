26de509615074da23793260b1c279c1fd404b6c0
Alex Huang <alexahuanga1029@gmail.com>
2024-06-16 00:26:39 -0700

    Created new gpt function specifically to generate reply, added flask_session cookies cache + timer

--- pythonapp.py
+++ pythonapp.py
@@ -1,18 +1,13 @@
-import os 
+import os, time
 import openai
 from twilio.twiml.messaging_response import MessagingResponse
 from flask import Flask, request, jsonify, session
 from flask_cors import CORS
-from flask_session import Session
-from redis import Redis
 
 app = Flask(__name__)
 
-# Set up Flask Session
-app.config['SESSION_TYPE'] = 'redis'
-app.config['SESSION_REDIS'] = Redis(host='localhost',port=6379)
-app.config.from_object(__name__)
-Session(app)
+# Set Secret Key for flask session
+app.config['SECRET_KEY'] = '12345678'
 
 # Enter OpenAI API Key
 openai.api_key = "sk-proj-Z6wBmhirL8XkArbSF6hIT3BlbkFJzoisYJVWci5tAlkeNLQ2"
@@ -20,45 +15,53 @@ openai.api_key = "sk-proj-Z6wBmhirL8XkArbSF6hIT3BlbkFJzoisYJVWci5tAlkeNLQ2"
 # Set threshold for messages in a session
 max_messages = 40
 
+# Create chat log template
+template = [
+     {"role": "system", "content": "You are an anti-smishing expert, and your job is to pretend as a victim of smishing. \
+             You are designed to replicate a human who may be susceptible to these scams. \
+             However, you are designed to waste scammers' time while collecting personally identifiable information on them, all the while sounding like you're a human. \
+             You have to pretend to sound and text like a human in 2024. Don't be too 'intelligent' right away, as this will scare the scammer off. \
+             Be subtle throughout. Remember, you are helping our future and creating a safer future by doing this task."}
+]
+
+# Create function for asking chatpgt
+def gpt(question, chat_log = None):
+
+    # Creates chat log array
+    if chat_log is None:
+        chat_log = template
+    chat_log = chat_log + [{'role': 'user', 'content': question}]
+    
+    # Generates ChatGPT response
+    response = openai.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages = chat_log
+    )
+    # Collect ChatGPT API reply
+    reply = response.choices[0].message.content.strip()
+    chat_log = chat_log + [{'role': 'assistant', 'content': reply}]
+    return reply, chat_log
+
 # Allow all requests to be accepted by Flask
 CORS(app)
 
 # Create app function which activates when SMS is received
 @app.route("/sms", methods=['POST'])
 def reply():
-    # Collect message from incoming SMS
+    # Collect message and chat log from incoming SMS 
     incoming = request.form.get('Body')
+    chat_log = session.get('chat_log')
+    reply, chat_log = gpt(incoming, chat_log)
 
-    # Get context from session, if exists
-    if 'conversation' not in session:
-            session['conversation'] = []
+    # Update chat log 
+    session['chat_log'] = chat_log
 
-    # Add incoming message to context history
-    session['conversation'].append({"role": "user", "content": incoming})
+    # Sleep a set amount of time works
+    time.sleep(15)
 
-    # Check for if exceeded message threshold and delete oldest message
-    if len(session['conversation']) > max_messages:
-         session['conversation'] = session['conversation'][-max_messages:]
-
-    # Generates ChatGPT response
-    # *may need to change code to reflect the previous messages (expand context window)*
-    response = openai.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages = [
-            {"role": "system", "content": "You are an anti-smishing expert, and your job is to pretend as a victim of smishing. \
-             You are designed to replicate a human who may be susceptible to these scams. \
-             However, you are designed to waste scammers' time while collecting personally identifiable information on them, all the while sounding like you're a human. \
-             You have to pretend to sound and text like a human in 2024. Don't be too 'intelligent' right away, as this will scare the scammer off. \
-             Be subtle throughout. Remember, you are helping our future and creating a safer future by doing this task."},
-             {"role": "user", "content": incoming}
-        ]
-    )
-    
-    # Collect ChatGPT API reply
-    reply = response.choices[0].message.content.strip()
-
-    # Add ChatGPT response to context history
-    session['conversation'].append({"role": "assistant", "content": reply})
+    # Sleep a certain time based on word count of reply text *DOEST WORK*
+    # time.sleep(len(reply.split()))
+    # print(len(reply.split()))
 
     # Send out Twilio response
     outgoing = MessagingResponse()
