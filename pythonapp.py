757e8834c2c4b735ba6ba6a5467c4ed2484faa09
Alex Huang <alexahuanga1029@gmail.com>
2024-06-30 17:46:33 -0700

    initialized connection with mongodb database, needs to be set up for calling from database

--- pythonapp.py
+++ pythonapp.py
@@ -1,13 +1,16 @@
-import os, time
+import os, time, datetime
 import openai
 from twilio.twiml.messaging_response import MessagingResponse
 from flask import Flask, request, jsonify, session
 from flask_cors import CORS
 from dotenv import load_dotenv
+from string import ascii_letters, punctuation, whitespace
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
 # Set Secret Key for flask session
+load_dotenv()
 app.config['SECRET_KEY'] = '12345678'
 
 # Enter OpenAI API Key
@@ -15,6 +18,14 @@ openai.api_key = os.getenv("OPENAI_KEY")
 
 # Set threshold for messages in a session
 max_messages = 40
+
+# Create ignore template to remove + from phone numers
+ignore = ascii_letters + punctuation + whitespace
+
+# Create MongoClient instance and dictionary of chatlogs
+client = MongoClient(os.getenv("MONGODB_STRING"))
+db = client.chatlogs
+message_database = db.messages
 
 # Create chat log template
 template = [
@@ -51,15 +62,29 @@ CORS(app)
 def reply():
     # Collect message and chat log from incoming SMS 
     incoming = request.form.get('Body')
+    # Access sender's phone number
+    phone_number = request.form.get('From')
+    # phone_number = str(incoming_id).translate(None, ignore)
+    # Access timestamp
+    # incoming_timestamp = datetime.utcnow()
+    message_database.insert_one({
+        "user": phone_number,
+        "role": 'user',
+        "content": incoming,
+    })
     chat_log = session.get('chat_log')
     reply, chat_log = gpt(incoming, chat_log)
-
+    # outgoing_timestamp = datetime.utcnow()
+    message_database.insert_one({
+        "user": phone_number,
+        "role": 'assistant',
+        "content": reply
+    })
+   
     # Update chat log 
     session['chat_log'] = chat_log
 
     # Sleep a set amount of time works
-    time.sleep(15)
-
     # Sleep a certain time based on word count of reply text *DOEST WORK*
     # time.sleep(len(reply.split()))
     # print(len(reply.split()))
@@ -69,7 +94,6 @@ def reply():
     outgoing.message(reply)
 
     return str(outgoing)
-    print(str(outgoing))
 
 # Run Flask app based on webhook
 if __name__ == "__main__":
